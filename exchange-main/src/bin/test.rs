ff020c1826413ec041691fe824d97397a6071b51
Yoon Dombo <ydb3448@gmail.com>
2025-02-15 13:25:00 +0800

    欧意example

--- exchange-main/src/bin/test.rs
+++ exchange-main/src/bin/test.rs
@@ -1,204 +1,184 @@
-extern crate mylib; 
+extern crate mylib;
+use futures::StreamExt;
 use log::info;
 use mylib::{
-    exchange::{factory, interface::send_request, types::ExchangeConfig},
-    tools::logger::init_logger,
+    exchange::{
+        consts::LONG,
+        factory::{self, new_ws_exg},
+        interface::send_request,
+        types::{ExchangeConfig, GridOrder, StdOrder, StdTicker, WsChannel},
+    },
+    tools::{logger::init_logger, util::gate_uuid},
 };
-
-use ethers::{signers::LocalWallet, types::H160};
-use hyperliquid_rust_sdk::{
-    BaseUrl, ClientLimit, ClientOrder, ClientOrderRequest, ExchangeClient, InfoClient, ExchangeResponseStatus, ExchangeDataStatus,ClientCancelRequestCloid,ClientCancelRequest
-};
-use tungstenite::client; 
-use std::str::FromStr;
-use std::{thread::sleep, time::Duration};
-use uuid::Uuid;
+use serde_yaml::Value;
 
 #[tokio::main]
-async fn main() { 
-    //fixme: aaaa 
-    // okx().await;
-    hyper().await; 
-}
-
-
-async fn okx() {
+async fn main() {
     let _g = init_logger();
     let symbol = "BTC-USDT";
     let exchange = "okx_usdt_swap";
     let account = "00";
-    let access_key = "660fb70b-2ed1-4ceb-8724-56312d006216";
-    let secret_key = "9EA688B308597E4FA8FCD841F2D2E3DF";
-    let password = "Ydb.15838890945";
+    let access_key = "";
+    let secret_key = "";
+    let password = "";
     let mut config = ExchangeConfig::new(symbol, access_key, secret_key, password);
     config.account = account.to_string();
     config.exchange = exchange.to_string();
 
     let mut rest = factory::new_rest_exg(exchange.to_string().clone(), config.clone());
 
-    let reqest = rest.get_market(symbol.to_string());
+    // 交易对配置
+    let request = rest.get_market(symbol.to_string());
     let client = rest.get_client();
-    let response = send_request(client, reqest).await;
+    let response = send_request(client, request).await;
+    info!("response: {:?}", response);
     let market = rest.parse_market(symbol.to_string(), &response.unwrap());
     info!("res: {:?}", market.unwrap());
-}
 
+    // 取消订单
+    let request = rest.cancel_all(symbol.to_string());
+    let client = rest.get_client();
+    let response = send_request(client, request).await;
+    info!("response: {:?}", response);
 
-async fn hyper() { 
-    let _g = init_logger();
-    let user: H160 = H160::from_str("0xd067e346e87Cb3cFDeb87f284e5451bE0970D403").unwrap();
-    let info_client = InfoClient::new(None, Some(BaseUrl::Mainnet)).await.unwrap();
+    // 取消订单
+    let request = rest.cancel_order(symbol.to_string(), "".to_string(), "".to_string());
+    let client = rest.get_client();
+    let response = send_request(client, request).await;
+    info!("response: {:?}", response);
 
-    // let spot_balance = info_client.user_token_balances(user).await.unwrap();
-    // info!("spot balance: {:?}", spot_balance);
+    //get account
+    let request = rest.get_account();
+    let client = rest.get_client();
+    let response = send_request(client, request).await;
+    let account = rest.parse_account(&response.unwrap());
+    info!("response: {:?}", account);
 
-    // let features_balance = info_client.user_state(user).await.unwrap();
-    // info!("features balance: {:?}", features_balance);
+    //get position
+    let request = rest.get_positions();
+    let client = rest.get_client();
+    let response = send_request(client, request).await;
+    let positions = rest.parse_positions(&response.unwrap());
+    info!("response: {:?}", positions);
 
-    // let fills = info_client.user_fills(user).await.unwrap();
-    // info!("fills: {:?}", fills);
- 
-    // let open_orders = info_client.open_orders(user).await.unwrap();
-    // info!("open orders: {:?}", open_orders);
+    //trade
+    let request = rest
+        .create_order(
+            symbol.to_string(),
+            LONG.to_string(),
+            90000.0,
+            0.01,
+            "limit".to_string(),
+            gate_uuid(),
+            serde_json::Value::Null,
+        )
+        .await;
+    let client = rest.get_client();
+    let response = send_request(client, request).await;
+    info!("response: {:?}", response);
 
-    // let user_state = info_client.user_state(user).await.unwrap();
-    // info!("user state: {:?}", user_state);
+    //batch trade
+    let order = GridOrder::new(LONG, 90000.0, 0.01, 0);
+    let request = rest.batch_create_order(vec![order], serde_json::Value::Null);
+    let client = rest.get_client();
+    let response = send_request(client, request).await;
+    info!("response: {:?}", response);
 
-    // let user_token_balances = info_client.user_token_balances(user).await.unwrap();
-    // info!("user token balances: {:?}", user_token_balances);
+    //amend
+    let request = rest
+        .amend_order(
+            symbol.to_string(),
+            91000.0,
+            0.01,
+            "".to_string(),
+            "".to_string(),
+            serde_json::Value::Null,
+        )
+        .await;
+    let client = rest.get_client();
+    let response = send_request(client, request).await;
+    info!("response: {:?}", response);
 
-    let meta_info = info_client.meta().await.unwrap();
-    info!("Metadata: {:?}", meta_info);
-  
+    // batch amend
+    let mut order = GridOrder::new(LONG, 90000.0, 0.01, 0);
+    order.client_id = "".to_string();
+    let request = rest.batch_amend_order(vec![order], serde_json::Value::Null);
+    let client = rest.get_client();
+    let response = send_request(client, request).await;
+    info!("response: {:?}", response);
 
-    // test_hype_limit_order().await; 
-    // order_and_cancel_cloid().await;
-}
+    let mut ws_client = new_ws_exg(config.exchange.clone(), config.clone());
+    let mut ticker_ws_client = ws_client.new(vec![WsChannel::Ticker]);
+    let mut ticker_ws = ticker_ws_client.get_connect().await;
 
-async fn test_hype_limit_order() {
-    let wallet: LocalWallet = "0x3959f4558de32d59e0271165dfb8782a732702701d7e3c79bdd56133ad4953c6"
-    .parse()
-    .unwrap();
+    let mut ws_client = new_ws_exg(config.exchange.clone(), config.clone());
+    let mut order_ws_client = ws_client.new(vec![WsChannel::Order]);
+    let mut order_ws = order_ws_client.get_connect().await;
 
-    let exchange_client = ExchangeClient::new(None, wallet, Some(BaseUrl::Mainnet), None, None)
-        .await
-        .unwrap();
+    let mut std_ticker = StdTicker::new();
+    let mut std_order = StdOrder::new();
 
-    let order = ClientOrderRequest {
-        asset: "ETH".to_string(), // 资产
-        is_buy: true,             // true 为做多, false 为做空
-        reduce_only: false,       // 只减仓
-        limit_px: 1800.0,         // 限价单
-        sz: 0.01,                 // 开仓数量
-        cloid: None,              // 客户端订单ID, 可选 可手动指定
-        order_type: ClientOrder::Limit(ClientLimit {
-            tif: "Gtc".to_string(),
-        }), // 订单类型, Limit 表示限价单, Gtc 表示一直有效直到取消
-    };
+    let mut is_init = false;
+    let orders: Vec<GridOrder> = vec![];
 
-    // 下单
-    let response = exchange_client.order(order, None).await.unwrap();
-    info!("Order placed: {response:?}");
+    let mut inerval = tokio::time::interval(tokio::time::Duration::from_secs(60));
+    inerval.tick().await;
 
-    let response = match response {
-        ExchangeResponseStatus::Ok(exchange_response) => exchange_response,
-        ExchangeResponseStatus::Err(e) => panic!("error with exchange response: {e}"),
-    };
-    // 获取订单ID
-    let status = response.data.unwrap().statuses[0].clone();
-    let oid = match status {
-        ExchangeDataStatus::Filled(order) => order.oid,
-        ExchangeDataStatus::Resting(order) => order.oid,
-        _ => panic!("Error: {status:?}"),
-    };
+    loop {
+        tokio::select! {
+            _ = inerval.tick() => {
+                is_init = false;
+            }
+            Some(Ok(msg)) = order_ws.next() =>{
+                if let Some(data) = (order_ws_client.order_handler)(&msg.to_string(), &mut std_order) {
+                    // receive websocket order, update local orders
+                    info!("recv order :{:?}", data);
+                }
+            }
+             Some(Ok(msg)) = ticker_ws.next() =>{
+                if let Some(data) = (ticker_ws_client.ticker_handler)(&msg.to_string(), &mut std_ticker) {
+                    info!("recv: {}", data.bid_price);
+                    if is_init == false {
+                        // init env begin
 
-    info!("Order ID: {oid}");
-}
+                        // get market
+                        // cancel orders
+                        // get account
+                        // get position
+                        // liqu other positions
+                        //
+                        // send orders
+                        is_init =true;
 
-async fn order_and_cancel_cloid() {
-    let wallet: LocalWallet = "3959f4558de32d59e0271165dfb8782a732702701d7e3c79bdd56133ad4953c6"
-        .parse()
-        .unwrap();
+                    } else {
+                        // check orders
+                        // if ticker changes,
+                        // cancel order / amend order / send order
+                    }
+                }
+            }
+        }
+        // if let Some(Ok(msg)) = ws.next().await {
+        //     if let Some(data) = (ws_client1.ticker_handler)(&msg.to_string(), &mut std_ticker) {
+        //         info!("recv: {}", data.bid_price);
+        //         if is_init == false {
+        //             // init env begin
 
-    let exchange_client = ExchangeClient::new(None, wallet, Some(BaseUrl::Mainnet), None, None)
-        .await
-        .unwrap();
+        //             // get market
+        //             // cancel orders
+        //             // get account
+        //             // get position
+        //             // liqu other positions
+        //             //
+        //             // send orders
+        //             is_init =true;
 
-    // Order and Cancel with cloid
-    let cloid = Uuid::new_v4();
-    let order = ClientOrderRequest {
-        asset: "ETH".to_string(),
-        is_buy: true,
-        reduce_only: false,
-        limit_px: 1800.0,
-        sz: 0.01,
-        cloid: Some(cloid),
-        order_type: ClientOrder::Limit(ClientLimit {
-            tif: "Gtc".to_string(),
-        }),
-    };
-
-    let response = exchange_client.order(order, None).await.unwrap();
-    info!("Order placed: {response:?}");
-
-    // So you can see the order before it's cancelled
-    sleep(Duration::from_secs(10));
-
-    let cancel = ClientCancelRequestCloid {
-        asset: "ETH".to_string(),
-        cloid,
-    };
-
-    // This response will return an error if order was filled (since you can't cancel a filled order), otherwise it will cancel the order
-    let response = exchange_client.cancel_by_cloid(cancel, None).await.unwrap();
-    info!("Order potentially cancelled: {response:?}");
-}
-
-
-async fn order_and_cancel() {
-let wallet: LocalWallet = "3959f4558de32d59e0271165dfb8782a732702701d7e3c79bdd56133ad4953c6"
-        .parse()
-        .unwrap();
-
-    let exchange_client = ExchangeClient::new(None, wallet, Some(BaseUrl::Testnet), None, None)
-        .await
-        .unwrap();
-
-    let order = ClientOrderRequest {
-        asset: "ETH".to_string(),
-        is_buy: true,
-        reduce_only: false,
-        limit_px: 1800.0,
-        sz: 0.01,
-        cloid: None,
-        order_type: ClientOrder::Limit(ClientLimit {
-            tif: "Gtc".to_string(),
-        }),
-    };
-
-    let response = exchange_client.order(order, None).await.unwrap();
-    info!("Order placed: {response:?}");
-
-    let response = match response {
-        ExchangeResponseStatus::Ok(exchange_response) => exchange_response,
-        ExchangeResponseStatus::Err(e) => panic!("error with exchange response: {e}"),
-    };
-    let status = response.data.unwrap().statuses[0].clone();
-    let oid = match status {
-        ExchangeDataStatus::Filled(order) => order.oid,
-        ExchangeDataStatus::Resting(order) => order.oid,
-        _ => panic!("Error: {status:?}"),
-    };
-
-    // So you can see the order before it's cancelled
-    sleep(Duration::from_secs(10));
-
-    let cancel = ClientCancelRequest {
-        asset: "ETH".to_string(),
-        oid,
-    };
-
-    // This response will return an error if order was filled (since you can't cancel a filled order), otherwise it will cancel the order
-    let response = exchange_client.cancel(cancel, None).await.unwrap();
-    info!("Order potentially cancelled: {response:?}");
+        //         } else {
+        //             // check orders
+        //             // if ticker changes,
+        //             // cancel order / amend order / send order
+        //         }
+        //     }
+        // }
+    }
 }
